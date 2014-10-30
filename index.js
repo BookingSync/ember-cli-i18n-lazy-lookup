f5cff1e4ae992b9b5f6a01df683213cef7f35cdf
Sébastien Grosjean <public@zencocoon.com>
2014-10-30 13:38:41 +0200

    Initial release

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+var i18nLazyLookup = require('broccoli-i18n-lazy-lookup');
+
 module.exports = {
   name: 'ember-cli-i18n-lazy-lookup'
+
+  included: function(app) {
+    app.registry.remove('template', 'broccoli-ember-hbs-template-compiler');
+    app.registry.add('template', {
+      name: 'ember-cli-i18n-lazy-lookup',
+      ext: 'hbs',
+      toTree: function(tree) {
+        return i18nLazyLookup(tree, options);
+      }
+    });
+    app.registry.add('template', 'broccoli-ember-hbs-template-compiler')
+  }
 };
